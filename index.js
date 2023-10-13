1b0ae834da2c5955a5c540cb8d75b57cc27d3ef5
Elijah Oquendo <elijahoquendo420@gmail.com>
2023-10-12 22:14:41 -0400

    first-class functions advanced

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution in this file!
+function returnFirstTwoDrivers(drivers) {
+    return drivers.slice(0, 2);
+  }
+
+  function returnLastTwoDrivers(drivers) {
+    return drivers.slice(-2);
+  }
+  
+  const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+  console.log(returnLastTwoDrivers(drivers));
+
+  const selectingDrivers = [
+    returnFirstTwoDrivers,
+    returnLastTwoDrivers
+  ];
+ 
+console.log(selectingDrivers[0](drivers)); 
+console.log(selectingDrivers[1](drivers)); 
+
+function createFareMultiplier(multiplier) {
+    return function(value) {
+      return value * multiplier;
+    };
+  }
+
+  function fareDoubler(fare) {
+    return fare * 2;
+  }
+  
+  function fareTripler(fare) {
+  return fare * 3; 
+  }
+
+  function selectDifferentDrivers(drivers, callback) {
+    return callback(drivers);
+  }
